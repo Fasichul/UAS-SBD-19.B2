343cc088f7cbce3a16af5bf7d8cf4b9f29122ee9
Fasichul <81569638+Fasichul@users.noreply.github.com>
2021-07-18 23:23:52 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,8 @@
+
+<?php require('header.php'); ?>
+<div class="content">
+    <h2>Ini Halaman HOME</h2>
+    <p>Tampilan ini adalah bagian content dari halaman.</p>
+</div>
+<?php require('footer.php'); ?>
+© 2021 GitHub, Inc.
